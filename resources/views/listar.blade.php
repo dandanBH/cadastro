5281fd7971f01ff92fd7773978914c7e82705a50
danilo de souza lima <danilo.lima@oi.net.br>
2021-06-26 11:25:03 -0300

    navegacao

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -3,12 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-md-left mt-5">
+       
         <div class="col-md-12">
+            
             <div class="card">
-                <div class="card-header">Listar Usuarios</div>
+            <div class="card-header">
+            <div class="col-md-9">
+                <a href ="{{url('/cadastro')}}">
+                    <button class="btn btn-success">Cadastrar Novo</button>
+                </a>
+            </div>
+            </div>
+              
+
                 <div class="card-body">
                     <ul class="list-group">
-                        <li class="list-group-item active">Cras justo odio</li>
+                        <li class="list-group-item">Cras justo odio</li>
                         <li class="list-group-item">Dapibus ac facilisis in</li>
                         <li class="list-group-item">Morbi leo risus</li>
                         <li class="list-group-item">Porta ac consectetur ac</li>
@@ -17,6 +27,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
